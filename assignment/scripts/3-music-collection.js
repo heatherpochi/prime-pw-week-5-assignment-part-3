2fcf1ce3018addfdcdd46a5a61c9d85dc47995ed
Heather Kim <heatherkim0405@gmail.com>
2022-01-11 20:09:27 -0600

    added six albums to the array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,22 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+  return album;
+}
+
+console.log('The first album is', addToCollection('Nevermind', 'Nirvana', 1991));
+console.log('The second album is', addToCollection('Renegades', 'RATM', 2000));
+console.log('The third album is', addToCollection('Absolution', 'Muse', 2003));
+console.log('The fourth album is', addToCollection('Bleach', 'Nirvana', 1989));
+console.log('The fifth album is', addToCollection('Parachutes', 'Coldplay', 2000));
+console.log('The sixth album is', addToCollection('Coming Up', 'Suede', 1996));
+
+console.log('My favorite albums are', collection);
