792b82b90b36c9132bc21ccd76b38be8df40ba7c
Heather Kim <heatherkim0405@gmail.com>
2022-01-11 21:44:29 -0600

    started working on the last stretch goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -30,3 +30,63 @@ function showCollection(array){
 }
 
 showCollection(collection);
+
+function findByArtist(artist){
+  let result = [];
+  for(let i = 0; i < collection.length; i++){
+    if(collection[i].artist === artist){
+      result.push(collection[i]);
+    }
+  }
+  return result;
+}
+
+console.log('The albums by Nirvana are',findByArtist('Nirvana'));
+console.log('The albnum by RATM is', findByArtist('RATM'));
+console.log('The albums by Pearl Jam are', findByArtist('Pearl Jam'));
+
+const criteria = {
+  artist: null,
+  year: null
+};
+
+function search(criteria){
+  let result = [];
+  for(let i = 0; i < collection.length; i++){
+    if(collection[i].artist === criteria.artist & collection[i].yearPublished === criteria.year){
+      result.push(collection[i]);
+    }
+  }
+  if( criteria === null || criteria === undefined){
+    return collection;
+  }
+  return result;
+}
+console.log(search(criteria));
+
+function addToCollectionUpdated(title, artist, yearPublished, tracks){
+  const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+    tracks = {
+      name: name,
+      duration: duration
+    }
+  }
+  collection.push(album);
+  return album;
+}
+
+const criteriaUpdated = {
+  artist: null,
+  year: null,
+  trackName: null,
+};
+
+function showCollectionUpdated(array){
+  console.log(array.length);
+  for(let i = 0; i < array.length; i++){
+    console.log(array[i].title + ' by ' + array[i].artist + ', published in ' + array[i].yearPublished:);
+  }
+}
